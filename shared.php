a6406fe6415c176535c7e9812a54e3ba6339985b
8stein <42@8stein.ru>
2014-12-27 16:00:24 +0300

    smth happened

--- shared.php
+++ shared.php
@@ -21,7 +21,7 @@ else
 }
 //$hash
 ?>
-<html lang="ru"><head>
+<html lang="ru"> <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
